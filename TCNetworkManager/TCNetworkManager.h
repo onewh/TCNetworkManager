25dccd03e5e4c3a926c9cb614c192904787bd7dc
王浩 <wh@addcn.com>
2018-08-03 10:24:41 +0800

    first commit

---
+++ TCNetworkManager/TCNetworkManager.h
@@ -0,0 +1,19 @@
+//
+//  TCNetworkManager.h
+//  TCNetworkManager
+//
+//  Created by onewh on 2018/8/3.
+//  Copyright © 2018年 onewh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TCNetworkManager.
+FOUNDATION_EXPORT double TCNetworkManagerVersionNumber;
+
+//! Project version string for TCNetworkManager.
+FOUNDATION_EXPORT const unsigned char TCNetworkManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TCNetworkManager/PublicHeader.h>
+
+
